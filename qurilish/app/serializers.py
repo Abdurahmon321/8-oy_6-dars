ec93aa5da83090e6d09e034e62df01b73f98b23a
Abdurahmon <abdurahmonhoshimov2004@gmail.com>
2024-06-02 02:09:44 +0500

    6-dars

---
+++ qurilish/app/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Hudud, QurilishTashkiloti, QurilishBinisi
+
+
+class HududSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hudud
+        fields = '__all__'
+
+
+class QurilishTashkilotiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QurilishTashkiloti
+        fields = '__all__'
+
+
+class QurilishBinisiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QurilishBinisi
+        fields = '__all__'
